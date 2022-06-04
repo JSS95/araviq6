f3f58ae7e4d220d30f9e1c6bf9effb26a19d4f4d
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-06-05 03:20:19 +0900

    maint: add basic files
    Add setup.py and basic package files

---
+++ arabesque6/__init__.py
@@ -0,0 +1,10 @@
+"""
+Arabesque6: NDArray - QVideoFrame Converter for Qt6
+===================================================
+
+Arabesque6 is a package to get :class:`numpy.ndarray` from ``QVideoFrame`` with
+Qt6 Python bindings - :mod:`PyQt6` and :mod:`PySide6`.
+
+"""
+
+from .version import __version__  # noqa

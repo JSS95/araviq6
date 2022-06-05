00c693c08000533d68efb4af8823d657697dbdd2
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-06-06 01:18:40 +0900

    refact: do not use QtDriver in dynqt6.py
    Instead, try import the module directly

--- arabesque6/dynqt6.py
+++ arabesque6/dynqt6.py
@@ -2,23 +2,18 @@
 Dynamically import PySide6 or PyQt6
 """
 
-import os
-from qimage2ndarray.qt_driver import QtDriver  # type: ignore[import]
+try:
+    from PySide6 import QtCore, QtGui, QtWidgets, QtMultimedia
+except ModuleNotFoundError:
+    from PyQt6 import QtCore, QtGui, QtWidgets, QtMultimedia  # type: ignore
+
+    QtCore.Signal = QtCore.pyqtSignal  # type: ignore
+    QtCore.Slot = QtCore.pyqtSlot  # type: ignore
 
 
-class Qt6Driver(QtDriver):
-    DRIVERS = ("PySide6", "PyQt6")
-    DEFAULT = "PySide6"
-
-    def __init__(self, drv=os.environ.get("QT_DRIVER")):
-        super().__init__(drv)
-        if self._drv == "PyQt6":
-            self.QtCore.Signal = self.QtCore.pyqtSignal
-            self.QtCore.Slot = self.QtCore.pyqtSlot
-
-
-Qt6 = Qt6Driver()
-QtCore = Qt6.QtCore
-QtGui = Qt6.QtGui
-QtWidgets = Qt6.QtWidgets
-QtMultimedia = Qt6.QtMultimedia
+__all__ = [
+    "QtCore",
+    "QtGui",
+    "QtWidgets",
+    "QtMultimedia",
+]

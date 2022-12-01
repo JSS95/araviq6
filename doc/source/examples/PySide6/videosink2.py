c154156d8521d46aa861c1cc189b3f3b3cc223eb
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-12-01 19:45:47 +0900

    feat: implement qimage to video frame
    Add videosink2.py

---
+++ doc/source/examples/PySide6/videosink2.py
@@ -0,0 +1,85 @@
+import cv2  # type: ignore[import]
+import numpy as np
+from PySide6.QtCore import Slot, QUrl
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout
+from PySide6.QtMultimedia import QMediaPlayer, QVideoSink, QVideoFrame
+from PySide6.QtMultimediaWidgets import QVideoWidget
+from araviq6 import VideoProcessWorker, VideoFrameProcessor, MediaController
+
+
+class CannyWorker(VideoProcessWorker):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._cannyMode = False
+
+    def setCannyMode(self, mode: bool):
+        self._cannyMode = mode
+
+    def processArray(self, array: np.ndarray) -> np.ndarray:
+        if self._cannyMode and array.size > 0:
+            gray = cv2.cvtColor(array, cv2.COLOR_RGB2GRAY)
+            canny = cv2.Canny(gray, 50, 200)
+            array = cv2.cvtColor(canny, cv2.COLOR_GRAY2RGB)
+        return array
+
+
+class Window(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._videoPlayer = QMediaPlayer()
+        self._playerVideoSink = QVideoSink()
+        self._frameProcessor = VideoFrameProcessor()
+        self._cannyWorker = CannyWorker()
+        self._videoWidget = QVideoWidget()
+        self._mediaController = MediaController()
+        self._cannyButton = QPushButton()
+
+        # set up the pipeline
+        self._videoPlayer.setVideoSink(self._playerVideoSink)
+        self._playerVideoSink.videoFrameChanged.connect(
+            self._frameProcessor.setVideoFrame
+        )
+        self._frameProcessor.videoFrameChanged.connect(self.displayVideoFrame)
+
+        self._mediaController.setPlayer(self._videoPlayer)
+        self._frameProcessor.setWorker(self._cannyWorker)
+        self._cannyButton.setCheckable(True)
+        self._cannyButton.toggled.connect(self._onCannyButtonToggle)
+
+        self._cannyButton.setText("Toggle edge detection")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self._videoWidget)
+        layout.addWidget(self._mediaController)
+        layout.addWidget(self._cannyButton)
+        self.setLayout(layout)
+
+    def _onCannyButtonToggle(self, state: bool):
+        self._cannyWorker.setCannyMode(state)
+        if self._videoPlayer.playbackState() != QMediaPlayer.PlaybackState.PlayingState:
+            self._frameProcessor.setVideoFrame(self._playerVideoSink.videoFrame())
+
+    def setSource(self, url: QUrl):
+        self._videoPlayer.setSource(url)
+
+    @Slot(QVideoFrame)
+    def displayVideoFrame(self, frame: QVideoFrame):
+        self._videoWidget.videoSink().setVideoFrame(frame)
+
+    def closeEvent(self, event):
+        self._frameProcessor.stop()
+        super().closeEvent(event)
+
+
+if __name__ == "__main__":
+    from araviq6 import get_samples_path
+    from PySide6.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    window = Window()
+    url = QUrl.fromLocalFile(get_samples_path("hello.mp4"))
+    window.setSource(url)
+    window.show()
+    app.exec()
+    app.quit()

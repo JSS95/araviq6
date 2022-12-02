587f40714f2da1e7acf47e262f85efec379d70e4
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-12-03 00:24:59 +0900

    fix: fix tester
    Try fix

--- araviq6/tests/test_videostream.py
+++ araviq6/tests/test_videostream.py
@@ -2,14 +2,14 @@ import cv2  # type: ignore[import]
 import numpy as np
 import qimage2ndarray  # type: ignore[import]
 import pytest
-from araviq6 import FrameToArrayConverter
+from araviq6 import qimage2qvideoframe, FrameToArrayConverter
 from araviq6.util import get_samples_path
 
 
 def test_qimage2qvideoframe(qtbot):
     array = cv2.imread(get_samples_path("hello.jpg"))
-    qimage2ndarray.array2qimage(array)
-    # image = qimage2ndarray.array2qimage(array)
+    image = qimage2ndarray.array2qimage(array)
+    qimage2qvideoframe(image)
     # frame = qimage2qvideoframe(image)
     # assert image == frame.toImage().convertToFormat(image.format())
 

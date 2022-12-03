21fabbb4ac9a5e91b3bc8bc24d31bcbfecd02592
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-12-03 19:48:45 +0900

    feat: add qimage2qvideoframe
    Modify test and ci

--- araviq6/tests/test_qimage2qvideoframe.py
+++ araviq6/tests/test_qimage2qvideoframe.py
@@ -7,21 +7,14 @@
 import cv2  # type: ignore[import]
 import qimage2ndarray  # type: ignore[import]
 from araviq6 import qimage2qvideoframe, get_samples_path
-from araviq6.qt_compat import QtGui, QtMultimedia
 
 
 def test_qimage2qvideoframe(qtbot):
-    image0 = qimage2ndarray.array2qimage(cv2.imread(get_samples_path("hello.jpg")))
-    assert not image0.isNull()
+    image1 = qimage2ndarray.array2qimage(cv2.imread(get_samples_path("hello.jpg")))
+    assert not image1.isNull()
 
-    for form in QtGui.QImage.Format:
-        image1 = image0.convertToFormat(form)
-        pixelFormat = QtMultimedia.QVideoFrameFormat.pixelFormatFromImageFormat(form)
-        if pixelFormat == QtMultimedia.QVideoFrameFormat.Format_Invalid:
-            continue
-
-        image2 = qimage2qvideoframe(image1).toImage()
-        image2.convertTo(image1.format())
-        assert not image2.isNull()
-        assert image1.format() == image2.format()
-        assert image1 == image2
+    image2 = qimage2qvideoframe(image1).toImage()
+    image2.convertTo(image1.format())
+    assert not image2.isNull()
+    assert image1.format() == image2.format()
+    assert image1 == image2

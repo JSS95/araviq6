3651666ce1f18c8b86c3e974254fcad08e5c7a23
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-12-04 01:19:17 +0900

    feat: add qimage2qvideoframe
    Modify test

--- araviq6/tests/test_array2qvideoframe.py
+++ araviq6/tests/test_array2qvideoframe.py
@@ -12,7 +12,7 @@ from araviq6.util import get_samples_path
 
 
 def test_array2qvideoframe(qtbot):
-    bgr_array = cv2.imread(get_samples_path("hello.jpg"))
+    bgr_array = cv2.imread(get_samples_path("hello.jpg"))[:1, :1]
     rgb_array = cv2.cvtColor(bgr_array, cv2.COLOR_BGR2RGB)
     frame = array2qvideoframe(bgr_array)
     assert np.all(qimage2ndarray.rgb_view(frame.toImage()) == rgb_array)

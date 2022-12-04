9db20dbf4013f4103de24c4096b94e3f05fb816e
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-12-04 13:23:49 +0900

    feat: add qimage2qvideoframe
    Enhance test

--- araviq6/tests/test_array2qvideoframe.py
+++ araviq6/tests/test_array2qvideoframe.py
@@ -13,11 +13,23 @@ def test_array2qvideoframe(qtbot):
     bgr_array1 = np.array([[[1]]], dtype=np.uint8)
     frame1 = array2qvideoframe(bgr_array1)
     assert np.all(
+        qimage2ndarray.byte_view(frame1.toImage())
+        == np.array([[[1, 1, 1, 255]]], dtype=np.uint8)
+    )
+    assert np.all(
         qimage2ndarray.rgb_view(frame1.toImage()) == np.array([[[1]]], dtype=np.uint8)
+    )
+    assert np.all(
+        qimage2ndarray.alpha_view(frame1.toImage())
+        == np.array([[[255]]], dtype=np.uint8)
     )
 
     bgr_array2 = np.array([[[1, 2]]], dtype=np.uint8)
     frame2 = array2qvideoframe(bgr_array2)
+    assert np.all(
+        qimage2ndarray.byte_view(frame2.toImage())
+        == np.array([[[1, 1, 1, 2]]], dtype=np.uint8)
+    )
     assert np.all(
         qimage2ndarray.rgb_view(frame2.toImage()) == np.array([[[1]]], dtype=np.uint8)
     )
@@ -28,12 +40,24 @@ def test_array2qvideoframe(qtbot):
     bgr_array3 = np.array([[[1, 2, 3]]], dtype=np.uint8)
     frame3 = array2qvideoframe(bgr_array3)
     assert np.all(
+        qimage2ndarray.byte_view(frame3.toImage())
+        == np.array([[[1, 2, 3, 255]]], dtype=np.uint8)
+    )
+    assert np.all(
         qimage2ndarray.rgb_view(frame3.toImage())
         == np.array([[[3, 2, 1]]], dtype=np.uint8)
+    )
+    assert np.all(
+        qimage2ndarray.alpha_view(frame3.toImage())
+        == np.array([[[255]]], dtype=np.uint8)
     )
 
     bgr_array4 = np.array([[[1, 2, 3, 4]]], dtype=np.uint8)
     frame4 = array2qvideoframe(bgr_array4)
+    assert np.all(
+        qimage2ndarray.byte_view(frame4.toImage())
+        == np.array([[[1, 2, 3, 4]]], dtype=np.uint8)
+    )
     assert np.all(
         qimage2ndarray.rgb_view(frame4.toImage())
         == np.array([[[3, 2, 1]]], dtype=np.uint8)

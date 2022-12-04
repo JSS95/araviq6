928577062d1ab5ee2d0b1d8c6c09f79afbe0ece3
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-12-04 13:14:07 +0900

    feat: add qimage2qvideoframe
    Add more test

--- araviq6/tests/test_array2qvideoframe.py
+++ araviq6/tests/test_array2qvideoframe.py
@@ -4,14 +4,40 @@
 # This issue happens only when any other test file is run before this one.
 # Should the user encounter such error, run the test for this file separately.
 
-import cv2  # type: ignore[import]
 import numpy as np
 import qimage2ndarray  # type: ignore[import]
 from araviq6 import array2qvideoframe
 
 
 def test_array2qvideoframe(qtbot):
-    bgr_array = np.array([[[1, 2, 3]]], dtype=np.uint8)
-    rgb_array = cv2.cvtColor(bgr_array, cv2.COLOR_BGR2RGB)
-    frame = array2qvideoframe(bgr_array)
-    assert np.all(qimage2ndarray.rgb_view(frame.toImage()) == rgb_array)
+    bgr_array1 = np.array([[[1]]], dtype=np.uint8)
+    frame1 = array2qvideoframe(bgr_array1)
+    assert np.all(
+        qimage2ndarray.rgb_view(frame1.toImage()) == np.array([[[1]]], dtype=np.uint8)
+    )
+
+    bgr_array2 = np.array([[[1, 2]]], dtype=np.uint8)
+    frame2 = array2qvideoframe(bgr_array2)
+    assert np.all(
+        qimage2ndarray.rgb_view(frame2.toImage()) == np.array([[[1]]], dtype=np.uint8)
+    )
+    assert np.all(
+        qimage2ndarray.alpha_view(frame2.toImage()) == np.array([[[2]]], dtype=np.uint8)
+    )
+
+    bgr_array3 = np.array([[[1, 2, 3]]], dtype=np.uint8)
+    frame3 = array2qvideoframe(bgr_array3)
+    assert np.all(
+        qimage2ndarray.rgb_view(frame3.toImage())
+        == np.array([[[3, 2, 1]]], dtype=np.uint8)
+    )
+
+    bgr_array4 = np.array([[[1, 2, 3, 4]]], dtype=np.uint8)
+    frame4 = array2qvideoframe(bgr_array4)
+    assert np.all(
+        qimage2ndarray.rgb_view(frame4.toImage())
+        == np.array([[[3, 2, 1]]], dtype=np.uint8)
+    )
+    assert np.all(
+        qimage2ndarray.alpha_view(frame4.toImage()) == np.array([[[4]]], dtype=np.uint8)
+    )
